2f83dcd40102b60926ffcd23ff0e7174ecd364c6
nnn-dream-vegas <naggymusic@gmail.com>
2019-03-09 19:49:08 +0900

    インクリメンタルサーチの機能のためコメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
+  # def index
+  #   @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+  #   respond_to do |format|
+  #     format.html
+  #     format.json
+  #   end
+  # end
 
 
   private
